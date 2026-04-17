057c6accebf10e6fcf2d94eed07b59ffa1f34d42
Varun <151656164+NVarun21@users.noreply.github.com>
2026-04-17 11:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (82.94%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,5 +1,9 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         a=0
         for num in nums:
             a=a^num
