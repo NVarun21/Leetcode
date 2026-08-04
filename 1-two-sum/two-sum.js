c11ee7366cef98eae07cca5f218e72e8b126f8b3
Varun <151656164+NVarun21@users.noreply.github.com>
2026-08-04 09:26:16 +0530

    Time: 4 ms (53.39%) | Memory: 55.6 MB (31.02%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let map=new Map();
     for(let i=0;i<nums.length;i++){
-
-    for(let j=i+1;j<nums.length;j++){
-        if(nums[i]+nums[j]==target){
-            return [i,j];
+        let required=target-nums[i];
+        if(map.has(required)){
+            return [map.get(required),i];
         }
-    }
+        map.set(nums[i],i);
     }
 };
