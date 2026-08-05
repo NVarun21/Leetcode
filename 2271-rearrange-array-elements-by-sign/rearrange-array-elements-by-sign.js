209fa25cf97c8d0e59b49d8592d4612edbf48e92
Varun <151656164+NVarun21@users.noreply.github.com>
2026-08-05 09:55:19 +0530

    Time: 29 ms (21.64%) | Memory: 100.7 MB (19.64%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    let neg_idx=1;
+    let pos_idx=0;
+    let res=[]
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>0){
+            res[pos_idx]=nums[i];
+            pos_idx+=2;
+        }
+        else if(nums[i]<0){
+            res[neg_idx]=nums[i];
+            neg_idx+=2;
+        }
+    }
+    return res;
+};
