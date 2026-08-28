6f3017cb47a118354df3589bd6d9146b6f5094ef
Varun <151656164+NVarun21@users.noreply.github.com>
2026-08-28 12:12:53 +0530

    Time: 51 ms (14.19%) | Memory: 80.1 MB (13.70%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -5,16 +5,10 @@
 var containsDuplicate = function(nums) {
     let map=new Map();
     for(let i=0;i<nums.length;i++){
-        let count=0;
         if(nums[i] in map){
-            count++;
-        }
-        else{
-            map[nums[i]]=i;
-        }
-        if(count>=1){
             return true;
         }
+        map[nums[i]]=i;
     }
     return false;
 };
