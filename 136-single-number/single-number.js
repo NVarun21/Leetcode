44ab35e0d668853405d09811cc2dd387cb889806
Varun <151656164+NVarun21@users.noreply.github.com>
2026-08-28 10:35:30 +0530

    Time: 352 ms (5.33%) | Memory: 56.5 MB (52.62%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,9 +3,20 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let a=0;
-    for(let num of nums){
-        a= a^num;
+    for(let i=0;i<nums.length;i++){
+        let count=0;
+        for(let j=0;j<nums.length;j++){
+            if(nums[i]==nums[j]){
+                count++;
+            }
+        }
+        if(count===1){
+            return nums[i];
+        }
     }
-    return a;
+    // let a=0;
+    // for(let num of nums){
+    //     a= a^num;
+    // }
+    // return a;
 };
