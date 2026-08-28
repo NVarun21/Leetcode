373124283ee1c419611c8da880ad9d7348304343
Varun <151656164+NVarun21@users.noreply.github.com>
2026-08-28 12:02:27 +0530

    Time: 8 ms (7.85%) | Memory: 56.9 MB (11.34%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,20 +3,34 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    let s=strs.sort();
-    let s1=s[0];
-    let s2=s[s.length-1];
-    let index=0;
-    while(index<s1.length){
-        if(s1[index]==s2[index]){
-            index++;
+    // let s=strs.sort();
+    // let s1=s[0];
+    // let s2=s[s.length-1];
+    // let index=0;
+    // while(index<s1.length){
+    //     if(s1[index]==s2[index]){
+    //         index++;
+    //     }
+    //     else{
+    //         break;
+    //     }
+    // }
+    // if(index==0){
+    //     return "";
+    // }
+    // return s1.slice(0,index);
+
+    let prefix=strs[0];
+    for(let i=1;i<strs.length;i++){
+        let j=0;
+        while(j<prefix.length && j<strs[i].length && prefix[j]==strs[i][j]){
+            j++;
         }
-        else{
-            break;
+        let temp="";
+        for(let k=0;k<j;k++){
+            temp+=prefix[k];
         }
+        prefix=temp;
     }
-    if(index==0){
-        return "";
-    }
-    return s1.slice(0,index);
+    return prefix;
 };
