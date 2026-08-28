82ea1f18befd7ebae1048fcde7badb98376d932a
Varun <151656164+NVarun21@users.noreply.github.com>
2026-08-28 11:18:30 +0530

    Time: 5 ms (55.69%) | Memory: 59.9 MB (94.16%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let roman_map={
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    };
+    let result=0;
+    for(let i=0;i<s.length;i++){
+        if(i+1<s.length && roman_map[s[i]]<roman_map[s[i+1]]){
+            result-=roman_map[s[i]];
+        }
+        else{
+            result+=roman_map[s[i]];
+        }
+    }
+    return result;
+};
