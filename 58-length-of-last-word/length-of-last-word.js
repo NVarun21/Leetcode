99d45ec97b05f04d5cbe2516dd9ed7a604e9b946
Varun <151656164+NVarun21@users.noreply.github.com>
2026-08-29 11:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (21.78%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,13 +3,23 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
+    // let count=0;
+    // s=s.trim();
+    // for(let i=s.length-1;i>=0;i--){
+    //     if(s[i]==" "){
+    //         break;
+    //     }
+    //     count+=1;
+    // }
+    // return count;
     let count=0;
-    s=s.trim();
-    for(let i=s.length-1;i>=0;i--){
-        if(s[i]==" "){
-            break;
-        }
-        count+=1;
+    let i=s.length-1;
+    while(i>=0 && s[i]==" "){
+        i--;
+    }
+    while(i>=0 && s[i]!==" "){
+        count++;
+        i--;
     }
     return count;
 };
