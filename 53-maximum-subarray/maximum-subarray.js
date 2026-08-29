ec98bf68bcb535ddb1a3e999e6b00988e99dcc5e
Varun <151656164+NVarun21@users.noreply.github.com>
2026-08-29 10:50:48 +0530

    Time: 6 ms (8.90%) | Memory: 65.8 MB (6.19%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -5,14 +5,14 @@
 var maxSubArray = function(nums) {
     let maxi=-Infinity;
     let s=0;
-    for(let num of nums){
+   for(let num of nums){
         s+=num;
         if(s>maxi){
-            maxi=s
+            maxi=s;
         }
         if(s<0){
             s=0;
         }
-    }
-    return maxi;
+   }
+   return maxi;
 };
