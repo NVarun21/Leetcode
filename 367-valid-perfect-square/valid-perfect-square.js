114b13fd49b196d300f342b5e30002128b5cfea1
Varun <151656164+NVarun21@users.noreply.github.com>
2026-08-30 16:34:55 +0530

    Time: 1 ms (24.18%) | Memory: 54.9 MB (14.89%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.js
+++ 367-valid-perfect-square/valid-perfect-square.js
@@ -3,19 +3,28 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    let left=0;
-    let right=num;
-    while(left<=right){
-        let mid=(left+right)>>1;
-        if(mid*mid==num){
-            return true;
-        }
-        else if(mid*mid<num){
-            left=mid+1;
-        }
-        else{
-            right=mid-1;
+    // let left=0;
+    // let right=num;
+    // while(left<=right){
+    //     let mid=(left+right)>>1;
+    //     if(mid*mid==num){
+    //         return true;
+    //     }
+    //     else if(mid*mid<num){
+    //         left=mid+1;
+    //     }
+    //     else{
+    //         right=mid-1;
+    //     }
+    // }
+    // return false;
+    if (num === 1){
+        return true;
+    }
+    for (let i=1;i*i<=num;i++) {
+        if (i*i===num){
+            return true
         }
     }
-    return false;
+    return false
 };
