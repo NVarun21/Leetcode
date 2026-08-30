4cc4041f36101c2a62b45da8180f1a0eeca802ef
Varun <151656164+NVarun21@users.noreply.github.com>
2026-08-30 16:16:05 +0530

    Time: 8 ms (47.36%) | Memory: 63 MB (16.24%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -2,21 +2,40 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
-    let arr=new Array(nums.length);
-    for(let i=0;i<nums.length;i++){
-        let sq=nums[i]**2;
-        arr[i]=sq;
-    }
+// var sortedSquares = function(nums) {
+//     let arr=new Array(nums.length);
+//     for(let i=0;i<nums.length;i++){
+//         let sq=nums[i]**2;
+//         arr[i]=sq;
+//     }
     
-    for(let i=0;i<nums.length;i++){
-        for(let j=0;j<nums.length-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                let temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
+//     for(let i=0;i<nums.length;i++){
+//         for(let j=0;j<nums.length-i-1;j++){
+//             if(arr[j]>arr[j+1]){
+//                 let temp=arr[j];
+//                 arr[j]=arr[j+1];
+//                 arr[j+1]=temp;
+//             }
+//         }
+//     }
+//     return arr;
+// };
+
+
+var sortedSquares = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    let ans = [];
+
+    for(let i=nums.length-1;i>=0;i--){
+        if(nums[left]*nums[left]>nums[right]*nums[right]){
+            ans[i]=nums[left]*nums[left];
+            left++;
+        }
+        else{
+            ans[i]=nums[right]*nums[right];
+            right--;
         }
     }
-    return arr;
+    return ans;
 };
