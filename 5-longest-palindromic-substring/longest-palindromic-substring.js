209421ed46d8aef4610c88fc2e54fe65b56d759d
Varun <151656164+NVarun21@users.noreply.github.com>
2026-09-03 09:54:52 +0530

    Time: 17 ms (86.48%) | Memory: 54.3 MB (91.10%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let start=0;
+    let maxLen=1;
+    function expand(left,right){
+        while(left>=0 && right<s.length && s[left]==s[right]){
+            if(right-left+1>maxLen){
+                start=left;
+                maxLen=right-left+1
+            }
+            left--;
+            right++;
+        }
+    }
+
+    for(let i=0;i<s.length;i++){
+        expand(i,i);
+        expand(i,i+1);
+    }
+
+    return s.substring(start,start+maxLen);
+}
+
